ec2fa2009264404f1ef8957fa72fc1135823ea48
karen <karemfreya@gmail.com>
2022-10-17 16:16:56 +0000

    primeros cambios

--- src/app.js
+++ src/app.js
@@ -7,5 +7,19 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let variable_excuse = "Hola Karen";
+
+  document.getElementById("excuse").innerHTML = variable_excuse;
+
   console.log("Hello Rigo from the console!");
 };
